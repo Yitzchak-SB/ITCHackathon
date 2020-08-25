3860f455ed09ca5ce00f901362176181e4445c21
izgong <36205068+izgong@users.noreply.github.com>
2020-08-25 11:44:16 +0300

    hello world

--- api.py
+++ api.py
@@ -1,2 +1,11 @@
 from flask import Flask, request
 from data.DataLayer import DataLayer
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World"
