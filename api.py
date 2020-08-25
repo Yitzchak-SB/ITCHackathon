3012c9529e7e9cd2d4fa9d9fb4b6bd2888389eaf
solomiia <solomia.ivychuk@gmail.com>
2020-08-25 17:14:20 +0300

    Added function for getting result by address_id

--- api.py
+++ api.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, json
 from Validations import Validations
-from data.SQLiteDataLayer import SqLiteDataLayer
+from data.SQLiteDataLayer import SqlDataLayer
 from flask_cors import CORS
 from decimal import *
 
 getcontext()
 
-data_layer = SqLiteDataLayer()
+data_layer = SqlDataLayer()
 app = Flask(__name__)
 cors = CORS(app)
 
@@ -16,10 +16,10 @@ def set_location():
     try:
         data = request.json
         print(data)
-        lat = float(data["data"]["lat"])
-        long = float(data["data"]["long"])
-        Validations.validate_lat(lat)
-        Validations.validate_long(long)
+        lat = data["data"]["lat"]
+        long = data["data"]["long"]
+        Validations.validate_lat(float(lat))
+        Validations.validate_long(float(long))
         data_layer.add_address(lat, long)
         return app.response_class(response=json.dumps({"message": "Data sent successfully to DataBase"}), status=200, mimetype="application/json")
     except Exception as e:
@@ -42,8 +42,11 @@ def set_email():
 
 @app.route("/")
 def get_result():
+    content = request.json
+    latitude = content["latitude"]
+    longitude = content["longitude"]
     try:
-        result = None
+        result = data_layer.get_result(latitude, longitude)
         return app.response_class(response=json.dumps({"result": result}), status=200,
                                   mimetype="application/json")
     except Exception as e:
--- data/SQLiteDataLayer.py
+++ data/SQLiteDataLayer.py
@@ -1,6 +1,6 @@
 from data.DataLayer import DataLayer
 import mysql.connector
-from decouple import config
+import decouple
 
 class SqlDataLayer(DataLayer):
     def __init__(self):
@@ -11,8 +11,8 @@ class SqlDataLayer(DataLayer):
         try:
             self.__my_sql = mysql.connector.connect(
                 host="127.0.0.1",
-                user=config('MYSQL_USER'),
-                password=config('PASSWORD'),
+                user="root",
+                password="Tvoiskazeniochi78!",
                 database='roofarm'
             )
         except Exception as e:
@@ -22,29 +22,63 @@ class SqlDataLayer(DataLayer):
         self.__my_sql.close()
 
     def add_address(self, lat, long):
-        cursor = self.__my_sql.cursor()
+
+        print([lat, long])
         try:
-            self.__my_sql.start_transaction()
-            sql = 'INSERT INTO addresses (lat, long) VALUES (%s, %s)'
-            values = (lat, long,)
+            cursor = self.__my_sql.cursor()
+            # self.__my_sql.start_transaction()
+            sql = "INSERT INTO roofarm.addresses (latitude, longitude) VALUES (%s, %s)"
+            values = (lat, long)
             cursor.execute(sql, values)
+            print("Success")
             self.__my_sql.commit()
             count = cursor.rowcount
-            return ("Inserted successfully " + count)
+            return ("Inserted successfully " + str(count))
         except Exception as e:
-            return e
+            print(e)
+        finally:
+            cursor.close()
+            self.__my_sql.close()
+
+    def get_result(self, latitude, longitude):
+        cursor = self.__my_sql.cursor()
+        try:
+            res = None
+            adr_id = None
+            sql_address = "SELECT idaddresses " \
+                          "FROM roofarm.addresses " \
+                          "WHERE latitude = %s" \
+                          "AND longitude = %s"
+            values_address = (latitude, longitude)
+            cursor.execute(sql_address, values_address)
+            for (address_id) in cursor:
+                adr_id = address_id
+                print(adr_id)
+            sql = "SELECT result FROM roofarm.results " \
+                  "JOIN roofarm.addresses " \
+                  "ON results.id_address = addresses.idaddresses " \
+                  "WHERE results.id_address = %s"
+            value = adr_id
+            cursor.execute(sql, value)
+            print("Success")
+            for (result) in cursor:
+                print(result)
+                res = result
+                print(res)
+            return res
         finally:
             cursor.close()
 
     def add_result(self, result, address_id):
         cursor = self.__my_sql.cursor()
         try:
-            sql = 'INSERT INTO results (res, id_address) VALUES (%s, %s)'
+            sql = "INSERT INTO results (res, id_address) VALUES (%s, %s)"
             values = (result, address_id)
             cursor.execute(sql, values)
             return "Inserted successfully " + cursor.rowcount
         finally:
             cursor.close()
+            self.__my_sql.close()
 
     def add_email(self, email, lat, long):
         cursor = self.__my_sql.cursor()
@@ -52,7 +86,7 @@ class SqlDataLayer(DataLayer):
             user_address_id = None
             user_result_id = None
             sql_address = 'SELECT idaddresses FROM addresses ' \
-                          'WHERE lat=%s, long=%s'
+                          'WHERE latitude=%s, longitude=%s'
             address_values = (lat, long)
             cursor.execute(sql_address, address_values)
             self.__my_sql.commit()
@@ -60,7 +94,7 @@ class SqlDataLayer(DataLayer):
                 user_address_id = id_address
             sql_result = 'SELECT id_address FROM roofarm.results ' \
                          'JOIN roofarm.addresses ' \
-                         'WHERE addresses.lat = %s AND addresses.long = %s'
+                         'WHERE addresses.latitude = %s AND addresses.long = %s'
             result_values = (lat, long)
             cursor.execute(sql_result, result_values)
             for (id_result) in cursor:
@@ -75,6 +109,7 @@ class SqlDataLayer(DataLayer):
             return e
         finally:
             cursor.close()
+            self.__my_sql.close()
 
     def get_all_data(self):
         cursor = self.__my_sql.cursor()
@@ -85,7 +120,7 @@ class SqlDataLayer(DataLayer):
                   "ON addresses.idaddresses = results.id_address " \
                   "INNER JOIN roofarm.users " \
                   "ON addresses.idaddresses = users.id_user_address;"
-            cursor.execute()
+            cursor.execute(sql)
             for (idaddresses, lat, long, idresults, result, id_address, idusers, email,
                  id_user_address, id_user_result) in cursor:
                 user[idusers] = {
@@ -97,4 +132,4 @@ class SqlDataLayer(DataLayer):
                 return user
         finally:
             cursor.close()
-            
+            self.__my_sql.close()
