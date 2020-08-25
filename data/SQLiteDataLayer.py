fbf2ed601068578bd3fddcdb17dd8cc72fca1bc8
solomiia <solomia.ivychuk@gmail.com>
2020-08-25 12:44:32 +0300

    Adding functions for inserting address and email to DB

--- data/SQLiteDataLayer.py
+++ data/SQLiteDataLayer.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import sqlite3
-
+from decouple import config
 from data.DataLayer import DataLayer
 
 
@@ -10,4 +10,50 @@ class SqlDataLayer(DataLayer):
         self.connect()
 
     def __connect(self):
-        pass
+        try:
+            self.__sqliteDb = sqlite3.connect(
+                host="127.0.0.1",
+                user=config('USER'),
+                password=config('PASSWORD'),
+                database='roofarm'
+            )
+        except Exception as e:
+            print(e)
+
+    def close(self):
+        self.__sqliteDb.close()
+
+    def add_user(self, lat, long):
+        cursor = self.__sqliteDb.cursor()
+        try:
+            self.__sqliteDb.start_transaction()
+            sql = 'INSERT INTO addresses (lat, long) VALUES (%s, %s)'
+            values = (lat, long,)
+            cursor.execute(sql, values)
+            self.__sqliteDb.commit()
+            count = cursor.rowcount
+            return ("Inserted successfully " + count)
+        except Exception as e:
+            return e
+        finally:
+            cursor.close()
+
+    def add_email_to_user(self, email, lat, long):
+        cursor = self.__sqliteDb.cursor()
+        try:
+            user_id = cursor.lastrowid
+            self.__sqliteDb.start_transaction()
+            sql_address = 'SELECT idaddresses FROM addresses ' \
+                          'WHERE lat=%s, long=%s'
+            values = (lat, long)
+            cursor.execute(sql_address, values)
+            sql_email = 'INSERT INTO users (email) VALUES (%s)'
+            value = (email,)
+
+            self.__sqliteDb.commit()
+            count = cursor.rowcount
+            return ("Inserted successfully " + count)
+        except Exception as e:
+            return e
+        finally:
+            cursor.close()
