2fca31d7c6191484923cb66e570339415d15005f
solomiia <solomia.ivychuk@gmail.com>
2020-08-25 11:03:56 +0300

    Started creating Flask app

---
+++ RooFarm/data/SQLiteDataLayer.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import sqlite3
+
+from data.DataLayer import DataLayer
+
+
+class SqlDataLayer(DataLayer):
+    def __init__(self):
+        super.__init__(self)
+        self.connect()
+
+    def __connect(self):
+        pass
