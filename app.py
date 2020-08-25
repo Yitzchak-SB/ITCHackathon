0ff20d087fe63bf75fcc808f9f770b376179aaa5
izgong <36205068+izgong@users.noreply.github.com>
2020-08-25 21:18:19 +0300

    result endpoint

--- app.py
+++ app.py
@@ -24,6 +24,18 @@ def set_location():
                                   mimetype="application/json")
 
 
+@app.route("/address", methods=["POST"])
+def set_address():
+    try:
+        data = request.json
+        return app.response_class(response=json.dumps({"message": data}), status=200,
+                                  mimetype="application/json")
+    except Exception as e:
+        print(e)
+        return app.response_class(response=json.dumps({"message": "Missing data for the request"}), status=400,
+                                  mimetype="application/json")
+
+
 @app.route("/email", methods=["POST"])
 def set_email():
     try:
